84fa865d5b75f9fb57f2e60912dca790a8ad5703
jovannil94 <jovanniluna@pursuit.org>
2021-05-28 13:59:36 -0400

    sets up env file

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { urlencoded } = require('body-parser');
-const PORT = 3000;
+const app = express();
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser,urlencoded({extended: false}));

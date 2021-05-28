0265d46ee9bc6092208e67c33efbd2e163e9e599
jovannil94 <jovanniluna@pursuit.org>
2021-05-28 13:39:54 -0400

    fixed timestamps for schema and seeds

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,26 @@
--- DROP DATABASE IF EXISTS raffle_database;
--- CREATE DATABASE raffle_database;
+DROP DATABASE IF EXISTS raffle_database;
+CREATE DATABASE raffle_database;
 
--- \c raffle_database;
+\c raffle_database;
 
-DROP TABLE IF EXISTS users ;
-DROP TABLE IF EXISTS raffles ;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS raffles;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY UNIQUE,
+    id SERIAL PRIMARY KEY,
     raffle_id INTEGER UNIQUE,
     firstname VARCHAR,
     lastname VARCHAR,
     email VARCHAR UNIQUE,
     phone VARCHAR UNIQUE,
-    registered_at TIMESTAMP
+    registered_at TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE raffles (
-    id INTEGER REFERENCES users(raffle_id) UNIQUE,
+    id INTEGER REFERENCES users(raffle_id) ON DELETE CASCADE,
     name VARCHAR UNIQUE,
     secret_token VARCHAR UNIQUE,
-    created_at TIMESTAMP,
+    created_at TIMESTAMP DEFAULT NOW(),
     raffled_at TIMESTAMP,
-    winner_id INTEGER REFERENCES users(id) UNIQUE,
+    winner_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ INSERT INTO users
     (id, raffle_id, firstname, lastname, email, phone)
 VALUES
     (1, 1, 'John', 'Doe', 'johndoe@gmail.com', '123-456-7891'),
-    (1, 1, 'Jane', 'Doe', 'janedoe@gmail.com', '987-654-3219');
+    (2, 2, 'Jane', 'Doe', 'janedoe@gmail.com', '987-654-3219');
 
 INSERT INTO raffles
     (id, name, secret_token)
